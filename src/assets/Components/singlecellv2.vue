9a09787c33c48031b84602db747347bc8e7db525
Caleb Oh <caleboh01@gmail.com>
2020-05-14 14:10:37 -0400

    added first diaganol (spelling?) check

--- src/assets/Components/singlecellv2.vue
+++ src/assets/Components/singlecellv2.vue
@@ -7,73 +7,125 @@ export default {
   data() {
     return {
       redActive: false,
-      yellowActive: false,
+      yellowActive: false
     };
   },
   props: {
     value: Object
   },
   methods: {
-    checkVertWin(){
-      if(this.value.y <= 2 && this.$board[this.value.x][this.value.y] == this.$board[this.value.x][this.value.y + 1] && this.$board[this.value.x][this.value.y] == this.$board[this.value.x][this.value.y + 2] && this.$board[this.value.x][this.value.y] == this.$board[this.value.x][this.value.y + 3]){
-        this.$winState.state = true
-        console.log('win')
+    //Have to refactor the checks to work with both players differently
+    checkVertWin() {
+      if (
+        this.value.y <= 2 &&
+        this.$board[this.value.x][this.value.y] ==
+          this.$board[this.value.x][this.value.y + 1] &&
+        this.$board[this.value.x][this.value.y] ==
+          this.$board[this.value.x][this.value.y + 2] &&
+        this.$board[this.value.x][this.value.y] ==
+          this.$board[this.value.x][this.value.y + 3]
+      ) {
+        this.$winState.state = true;
+        console.log("win");
       }
     },
-    checkHorWin(){
-      let streak = 0
-      let streak_value = 'yellow'
-      for(let i = 0; i<7; i++){
-        if(this.$board[i][this.value.y] == this.streak_value){
-          this.streak++
-          if(this.streak == 4){
-            console.log('win')
-            this.$winState.state = true
-            break
+    checkHorWin() {
+      let streak = 0;
+      let streak_value = "yellow";
+      for (let i = 0; i < 7; i++) {
+        if (this.$board[i][this.value.y] == this.streak_value) {
+          this.streak++;
+          if (this.streak == 4) {
+            console.log("win");
+            this.$winState.state = true;
+            break;
           }
         } else {
-          this.streak = 1
-          if(this.$board[i][this.value.y] != ''){
-          this.streak_value = this.$board[i][this.value.y]
+          this.streak = 1;
+          if (this.$board[i][this.value.y] != "") {
+            this.streak_value = this.$board[i][this.value.y];
           }
         }
       }
     },
+    checkDiagWin() {
+      let streak = 0;
+      let streak_value = "yellow";
+      for (let i = 0; i < 7; i++) {
+        if (this.$board[this.value.x - 3 + i][this.value.y - 3 + i] == null) {
+          return;
+        } else if (
+          this.$board[this.value.x - 3 + i][this.value.y - 3 + i] ==
+          this.streak_value
+        ) {
+          this.streak++;
+          if (this.streak == 4) {
+            console.log("win");
+            this.$winState.state = true;
+            break;
+          }
+        } else {
+          this.streak = 1;
+          if (this.$board[this.value.x - 3 + i][this.value.y - 3 + i] != "") {
+            this.streak_value = this.$board[this.value.x - 3 + i][
+              this.value.y - 3 + i
+            ];
+          }
+        }
+      }
+      
+    },
     myPlay() {
-      if (this.$player1turn.value == true && this.$board[this.value.x][this.value.y] == '' && this.$board[this.value.x][(this.value.y)+1] != '') {
+      if (
+        this.$player1turn.value == true &&
+        this.$board[this.value.x][this.value.y] == "" &&
+        this.$board[this.value.x][this.value.y + 1] != ""
+      ) {
         this.redActive = true;
         this.yellowActive = false;
         this.$player1turn.value = !this.$player1turn.value;
         console.log("red");
-        this.$board[this.value.x][this.value.y] = 'red'
-        this.checkVertWin()
-        this.checkHorWin()
-      } else if (this.$player1turn.value == false && this.$board[this.value.x][this.value.y] == '' && this.$board[this.value.x][(this.value.y)+1] != ''){
+        this.$board[this.value.x][this.value.y] = "red";
+        this.checkVertWin();
+        this.checkHorWin();
+      } else if (
+        this.$player1turn.value == false &&
+        this.$board[this.value.x][this.value.y] == "" &&
+        this.$board[this.value.x][this.value.y + 1] != ""
+      ) {
         this.yellowActive = true;
         this.redActive = false;
         this.$player1turn.value = !this.$player1turn.value;
         console.log("yellow");
-        this.$board[this.value.x][this.value.y] = 'yellow'
-        this.checkVertWin()
-        this.checkHorWin()
-      } else if (this.$player1turn == true && this.value.y == 5 && this.$board[this.value.x][this.value.y]== '') {
+        this.$board[this.value.x][this.value.y] = "yellow";
+        this.checkVertWin();
+        this.checkHorWin();
+      } else if (
+        this.$player1turn == true &&
+        this.value.y == 5 &&
+        this.$board[this.value.x][this.value.y] == ""
+      ) {
         this.redActive = true;
         this.yellowActive = false;
         this.$player1turn.value = !this.$player1turn.value;
         console.log("red");
-        this.$board[this.value.x][this.value.y] = 'red'
-        this.checkVertWin()
-        this.checkHorWin()
-      } else if (this.$player1turn == false && this.value.y == 5 && this.$board[this.value.x][this.value.y]== ''){
+        this.$board[this.value.x][this.value.y] = "red";
+        this.checkVertWin();
+        this.checkHorWin();
+      } else if (
+        this.$player1turn == false &&
+        this.value.y == 5 &&
+        this.$board[this.value.x][this.value.y] == ""
+      ) {
         this.redActive = true;
         this.yellowActive = false;
         this.$player1turn.value = !this.$player1turn.value;
         console.log("red");
-        this.$board[this.value.x][this.value.y] = 'yellow'
-        this.checkVertWin()
-        this.checkHorWin()
+        this.$board[this.value.x][this.value.y] = "yellow";
+        this.checkVertWin();
+        this.checkHorWin();
       }
-    },
+    }
   }
 };
 </script>
